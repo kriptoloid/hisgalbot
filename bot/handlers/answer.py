feacb25a5c818ec4830a31f084d5b7c17478d8fe
kriptoloid <414maksiml414@gmail.com>
2024-04-15 21:53:21 +0300

    Add command 'cancel'

--- bot/handlers/answer.py
+++ bot/handlers/answer.py
@@ -7,6 +7,12 @@ from aiogram.filters import Command
 
 router = Router()
 
+@router.message(Command("cancel"))
+async def cancel_answer(msg: Message, state: FSMContext):
+    await state.clear()
+    await message.answer(
+        text="Відповідь скасована"
+    )
 
 @router.callback_query(F.data.startswith("rm"))
 async def send_random_value(callback: CallbackQuery, state: FSMContext,):
@@ -18,4 +24,9 @@ async def send_random_value(callback: CallbackQuery, state: FSMContext,):
 async def answer(msg: Message, state: FSMContext, bot: Bot):
     user_data = await state.get_data()
     await msg.send_copy(user_data['user_id'])
+    await state.clear()
+
+@router.message(Command("cancel"))
+async def cancel_answer(msg: Message, state: FSMContext):
+    print("ok")
     await state.clear()
